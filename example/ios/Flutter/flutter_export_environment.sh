2cf389cf9f0fa294b210f5d0b08632907c18eadf
shaoqing <zhangshaoqing@idcm.io>
2021-03-26 13:11:37 +0800

    first

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/huangyi/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/huangyi/Downloads/flutter_swiper-master/example"
+export "FLUTTER_TARGET=/Users/huangyi/Downloads/flutter_swiper-master/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/huangyi/Downloads/flutter_swiper-master/example/.dart_tool/package_config.json"
